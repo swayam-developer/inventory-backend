c8f026a6604cf29ff8a2ce30347c0a618a5fe015
swayam-developer <swayamsathawane@gmail.com>
2025-06-01 22:02:45 +0530

    Port updated

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -30,7 +30,7 @@ app.use("/products", productRoutes_1.default); //http://localhost:8000/products
 app.use("/users", userRoutes_1.default); //http://localhost:8000/users
 app.use("/expenses", expenseRoutes_1.default); //http://localhost:8000/expenses
 // Server
-const port = Number(process.env.PORT) || 3001;
-app.listen(port, '0.0.0.0', () => {
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
     console.log(`server is running on ${port}`);
 });
